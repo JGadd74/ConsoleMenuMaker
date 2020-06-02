8309df157630f5017229434a4c7e16279a49bc43
JGadd74 <jon-k-g94@hotmail.com>
2020-06-01 20:26:57 -0400

    First commit

---
+++ MenuFramework/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Logging;
+
+namespace MenuFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger myLogger = new Logger();
+            myLogger.RunLogger();
+        }
+    }
+}
