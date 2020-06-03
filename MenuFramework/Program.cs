5fc308371393f2369709bf9d9cf3842d495f0671
JGadd74 <jon-k-g94@hotmail.com>
2020-06-03 11:17:41 -0400

    Added GetScrollSelection to get the most recent selection info

--- MenuFramework/Program.cs
+++ MenuFramework/Program.cs
@@ -1,13 +1,31 @@
 ﻿using System;
-using Logging;
+
 
 namespace MenuFramework
 {
     class Program
     {
+
+        static int nmbr = 0;
+        static Menu TestMenu = new Menu(MenuType.NmbrChoices, "Test " + Convert.ToString(nmbr));
+
+        static bool updateSelection()
+        {
+            nmbr = TestMenu.GetCurrentScrollSelection();
+            TestMenu.SetHeaderPrompt("Test " + Convert.ToString(nmbr));
+            return true;
+        }
         static void Main(string[] args)
         {
+
+            TestMenu.addMethod(updateSelection, "Test");
+            TestMenu.addMethod(updateSelection, "Test");
+
+
+            TestMenu.runMenu();
             
         }
+
+     
     }
 }
